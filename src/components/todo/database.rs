e2d368bb28a1dede24577a559cfafef0cfae4238
electricsteve <96793824+electricsteve@users.noreply.github.com>
2026-03-21 23:26:27 +0100

    start of query

--- src/components/todo/database.rs
+++ src/components/todo/database.rs
@@ -4,11 +4,10 @@ use surrealdb::Surreal;
 use super::constants::COMPONENT_ID;
 
 pub async fn migrate(db: &Surreal<Db>) -> Result<(), surrealdb::Error> {
-    db.query(
-        "
-
-        ",
-    ).await?;
+    db.query("
+DEFINE TABLE IF NOT EXISTS todo SCHEMAFULL;
+DEFINE FIELD IF NOT EXISTS list ON TABLE todo TYPE array<string>;
+        ",).await?;
     Ok(())
 }
 
